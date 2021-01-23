a55f51ac871011bcb0bd23de462769831511b08f
Runar Ingebrigtsen <runar@rin.no>
2021-01-23 14:50:23 +0100

    use same origin

--- src/BloodAPI.js
+++ src/BloodAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://localhost:5001/api",
 	headers: {
 	  "Content-type": "application/json"
 	},
